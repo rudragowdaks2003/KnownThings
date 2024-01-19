34457ad4ad078c4438a059abe6f8774a0bbce46e
rudragowdaks2003 <115013638+rudragowdaks2003@users.noreply.github.com>
2024-01-19 23:28:46 +0530

    updated
    C++ Program for implementation of searching technique - both Linear and Binary searching

--- B_L_search.cpp
+++ B_L_search.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 int linearsearch(int arr[],int key,int n){
 	for(int i=0;i<n;i++){
 		if(arr[i]==key){
@@ -9,41 +8,43 @@ int linearsearch(int arr[],int key,int n){
 	}
 return -1;
 }
-
-
 int binarysearch(int arr[],int key,int n){
 	int l=0,r=n;
+	while(l<=r){
 	int mid=(l+r)/2;
-	if(arr[mid]==key)
+	if(arr[mid]==key){
 		return mid;
-		if(arr[mid]>key)
+	}
+	else if(arr[mid]>key){
+		r=mid-1;
+	}
+	else 
 		l=mid+1;
-		else r=mid-1;
+	}
 	return -1;
 }
 
 int main(){
  	int n,arr[50],search;
+	cout<<"Enter the size of array: ";
  	cin>>n;
  	
  	for(int i=0;i<n;i++){
+		cout<<"Element ["<<i<<"] : ";
  		cin>>arr[i];
 	 }
-	 
 	 int key;
+	 cout<<"Enter the element/key to search: ";
 	 cin>>key;
-	 
-	 
-	 cout<<"enter to select searching:";
+	 cout<<"\n1.Linear Search\n2.Binary search\nEnter to select searching:";
 	 cin>>search;
  if(search==1){
-		cout<<linearsearch(arr,key,n);
+	cout<<"Element found at ["<<linearsearch(arr,key,n)<<"] using Linear Search.\n";
 }
 
  if(search==2){
-	cout<<binarysearch(arr,key,n);
+	cout<<"Element found at ["<<binarysearch(arr,key,n)<<"] using Binary search\n";
 }
 	 
 	 return 0;
  }
-
